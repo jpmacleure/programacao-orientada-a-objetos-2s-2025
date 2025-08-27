742465b4d9e9938cad09f9c24da562e4d3dec8f4
jpmacleure <macleure-@hotmail.com>
2025-08-27 12:39:22 -0300

    add sistema radar

---
+++ sistema_radar/Radar.java
@@ -0,0 +1,18 @@
+public class Radar {
+    public Integer limiteVelocidade;
+    public String modelo;
+    public String localizacao;
+
+    public void emitirNotificacao(String placa, Integer valocidadeAvaliada){
+        System.out.println("Limite de velocidade: "+ this.limiteVelocidade);
+        System.out.println("Velocidade avaliada: "+ valocidadeAvaliada);
+        System.out.println("Placa: "+placa);
+    }
+
+    public void avaliarVelocidade(Carro c){
+        if(c.velocidade > this.limiteVelocidade){
+            emitirNotificacao(c.placa, c.velocidade);
+        }
+    }
+
+}
