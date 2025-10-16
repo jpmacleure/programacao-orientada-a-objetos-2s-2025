5b02ff494f69a1bc79f1db189e2858d522564311
jpmacleure <macleure-@hotmail.com>
2025-10-16 11:26:56 -0300

    add sistema_biblioteca

---
+++ sistema_biblioteca/Emprestimo.java
@@ -0,0 +1,54 @@
+public class Emprestimo {
+    private TomadorEmprestimo tomadorEmprestimo;
+    private Material material;
+    private String dataEmprestimo;
+    private String dataDevolucao;
+
+    public Emprestimo(TomadorEmprestimo tomadorEmprestimo, Material material, String dataEmprestimo, String dataDevolucao) {
+        this.tomadorEmprestimo = tomadorEmprestimo;
+        this.material = material;
+        this.dataEmprestimo = dataEmprestimo;
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public TomadorEmprestimo getTomadorEmprestimo() {
+        return this.tomadorEmprestimo;
+    }
+
+    public void setTomadorEmprestimo(TomadorEmprestimo tomadorEmprestimo) {
+        this.tomadorEmprestimo = tomadorEmprestimo;
+    }
+
+    public Material getMaterial() {
+        return this.material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public String getDataEmprestimo() {
+        return this.dataEmprestimo;
+    }
+
+    public void setDataEmprestimo(String dataEmprestimo) {
+        this.dataEmprestimo = dataEmprestimo;
+    }
+
+    public String getDataDevolucao() {
+        return this.dataDevolucao;
+    }
+
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public void exibirDetalhes(){
+        System.out.println("Data do empréstimo: " + this.dataEmprestimo);
+        System.out.println("Data da devolução: " + this.dataDevolucao);
+        System.out.println("Pessoa vinculada");
+        this.tomadorEmprestimo.exibirInfo();
+        System.out.println("Material vinculada");
+        this.material.descricao();
+    }
+}
